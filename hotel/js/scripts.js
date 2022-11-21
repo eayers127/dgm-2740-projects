e8a7a1ba0889ed1f8bcfcfd93436ffd2c1d96111
Ethan Ayers <ethan.ayers88@gmail.com>
2022-11-21 15:01:38 -0700

    added hotel page

---
+++ hotel/js/scripts.js
@@ -0,0 +1,62 @@
+// what is the path to the JSON file?
+const apiURL = "./hoteldata.json";
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((myList) => {
+    //Once it comes back, display it to the console.
+    console.log(myList);
+    
+for(i = 0; i < myList.length; i++){
+
+  let theIconLeftTag = document.createElement("ion-icon");
+  theIconLeftTag.className = "icon";
+
+  let addressTag = document.createElement("p");
+  addressTag.textContent = myList[i].address[0];
+
+  let cityTag = document.createElement("p");
+  cityTag.textContent = myList[i].address[1];
+
+  let phoneTag = document.createElement("p");
+  phoneTag.textContent = myList[i].phone;
+
+  let cardInfo = document.createElement("div");
+  cardInfo.className = "info";
+  cardInfo.appendChild(addressTag);
+  cardInfo.appendChild(phoneTag);
+  cardInfo.appendChild(cityTag);
+  
+
+
+    let myImageTag = document.createElement("img");
+    myImageTag.src = myList[i].photo;
+
+    let myCaptionTag = document.createElement("figcaption");
+    myCaptionTag.textContent = myList[i].name;
+
+    let myFigureTag = document.createElement("figure");
+    myFigureTag.appendChild(myImageTag);
+    myFigureTag.appendChild(myCaptionTag);
+    
+    let divWrapper = document.createElement("div");
+    divWrapper.className = "cardWrapper";
+    document.getElementById('myCards').appendChild(myFigureTag);
+    document.getElementById('myCards').appendChild(cardInfo);
+
+    
+}
+
+    // myFigureTag.className = "happyCamper";
+    // let theIconLeftTag;
+    // theIconLeftTag.className = "icon"
+    
+    
+    
+    
+    
+}); //end of "then" fat arrow function
+
+let myTag = document.createElement("p");
+myTag.textContent="hello world";
